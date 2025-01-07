de52783cb9825292b5f2e1ffbe3d78945f27c6b5
Damian Lukowski <github-commit@arcsin.de>
2025-01-07 21:24:53 +0100

    Switch to xz

--- .github/scripts/before_build.sh
+++ .github/scripts/before_build.sh
@@ -6,8 +6,8 @@ export DEBEMAIL="nobody@localhost"
 
 apt-get -y install devscripts
 
-url="https://download-installer.cdn.mozilla.net/pub/devedition/releases/$package_version/linux-x86_64/de/firefox-$package_version.tar.bz2"
+url="https://download-installer.cdn.mozilla.net/pub/devedition/releases/$package_version/linux-x86_64/de/firefox-$package_version.tar.xz"
 mkdir download
-curl -s -o - $url | tar -C download --strip-components=1 -jxf -
+curl -s -o - $url | tar -C download --strip-components=1 -Jxf -
 
 dch --create --newversion $package_version --package firefox-dev --distribution unstable
--- .github/scripts/package_version.sh
+++ .github/scripts/package_version.sh
@@ -5,7 +5,7 @@ set -e
 base_url="https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=de"
 redirect_url=$( curl -s -o /dev/null -w '%{redirect_url}' $base_url )
 
-version=$( echo $redirect_url | grep -oP '(?<=firefox-)(.+)(?=.tar.bz2)' )
+version=$( echo $redirect_url | grep -oP '(?<=firefox-)(.+)(?=.tar.xz)' )
 
 git fetch --depth 1 --tags
 
